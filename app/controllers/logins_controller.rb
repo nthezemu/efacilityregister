cf28a9aef7dffe998d5c98245959c3f6ff412017
lytonnyemba <lyton.m.nyemba@gmail.com>
2017-08-31 13:29:17 +0200

    login controller added

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,65 +1,35 @@
 class LoginsController < ApplicationController
   skip_before_filter :perform_basic_auth, :only => :logout
-
+  
   def login
-    @coa = icoFolder("coa")
-    logout!
+    
   end
+  
+  def authenticate
+    # parameters = params.require(:personel).permit(:username, :password)
+     
+      #username = parameters['username'
+     # password = parameters['password']
+    username = params[:personel][:username]
+    password = params[:personel][:password]
 
-  def create
-
-    username = params[:user][:username]
-    password = params[:user][:password]
-
-    #user = User.get_active_user(username)
-    user = User.where(username: username, active: 1).first
+    personel = Personel.where(username: username).first
     
 
-   if username.present? and password.present?
-    if user and user.password_matches?(password)
-      login! user
-      redirect_to "/", referrer_param => referrer_path and return
+  # if username.present? and password.present?
+    if personel and personel.password_matches?(password)
+     
+      redirect_to :action => 'index'
 
       
-    elsif (User.where(username: username, active: 0).first)
-        flash[:error] = 'The user account is deactivated. Please contact your Administrator.'
-        redirect_to "/login" and return
+    elsif 
+        flash[:error] = 'The username or password is incorrect. Please contact your Administrator.'
+        redirect_to :action => 'login'
     end
 
-   end
-		flash[:error] = 'That username and/or password is not valid.'
-      	redirect_to "/login"
-  end
-
-  def logout
-    # session[:touchcontext] = nil
-    logout!
-=begin     
-    unless SETTINGS['app_gate_url'].blank?
-      
-      redirect_to SETTINGS['app_gate_url'].to_s
-    else
-      flash[:notice] = 'You have been logged out. Good Bye!'
-      redirect_to "/", referrer_param => referrer_path
-    end
-=end
-    redirect_to "/login"
-  end
-
-  
-
-  protected
-
-  def default_path
-    '/'
-  end
-
-  def perform_basic_auth
-    authorize! :access, :login
-  end
-
-  def access_denied
-    redirect_to default_path
+   #end
+		#flash[:error] = 'That username and/or password is not valid.'
+      	#redirect_to "/login"
   end
 
 end
