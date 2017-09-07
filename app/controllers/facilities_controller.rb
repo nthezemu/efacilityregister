bb4023da1a8a1a0c1612b313e13913bd593e04de
alice <amusukwa@gmail.com>
2017-09-07 16:14:26 +0200

    styling search

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -67,23 +67,11 @@ class FacilitiesController < ApplicationController
     end 
    end
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-   def list
->>>>>>> 0a7d1b91742f851ca10c7104d6ecad0cf20ecbd3
 
-=======
-
-  
-
-
-  
   
   
   
    def list
->>>>>>> e80521b1afb83362ba25612ad2d774bd4fa4e4fb
     if params[:name].present?
       particular_value = params[:name]
       @specific_name = params[:name]
@@ -91,13 +79,6 @@ class FacilitiesController < ApplicationController
        fs on f.facility_id=fs.facility_id left join services s on fs.service_id =s.id where s.name = '#{particular_value}'")
 
     else
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 76717ffdd0044e01f835fa41cf55878483e1ce79
->>>>>>> e80521b1afb83362ba25612ad2d774bd4fa4e4fb
->>>>>>> 0a7d1b91742f851ca10c7104d6ecad0cf20ecbd3
     facilities1 = session[:facilities_queried]
     if facilities1.blank?
 
