49a8cc83152a3e26a9fb0d31a06a0ad9866443a0
Blessings Nthezemu Kamanga <blessnkamanga@gmail.com>
2017-09-11 14:03:40 +0200

    have resolved some conflicts in list.html

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,28 +26,28 @@ Service.create(name: 'family planning')
 Service.create(name: 'Maternity')
 Service.create(name: 'Dental')
 
-Facility.create(facility_id: 'll01001', name: 'bwaila', description: 'This is in Lilongwe', cell_location: 'Lilongwe', closing_date: '2070-10-10', opening_date: '2017
+Facility.create(facility_id: 'LL01001', name: 'bwaila', description: 'This is in Lilongwe', cell_location: 'Lilongwe', closing_date: '2070-10-10', opening_date: '2017
 	-10-10', parent_facility: 'Kamuzu Central Hospital', email_address: 'bwaila@gmail.com', phone_number: '+26512345612', type_code: 'hospital', latitude: '33.77561', longitude: '-13.99164', status: 'Functional', service_id: '1', created_by: 'Blessings', approved_by: 'Lyton',created_at:  '2017-10-10', updated_at: '2017-10-10');
 
-Facility.create(facility_id: 'll01002', name: 'Area 25 Health centre', description: 'This is in area 25', cell_location: 'Lilongwe', closing_date: '2070-10-10', opening_date: '2017
+Facility.create(facility_id: 'LL01002', name: 'Area 25 Health centre', description: 'This is in area 25', cell_location: 'Lilongwe', closing_date: '2070-10-10', opening_date: '2017
 	-10-10', parent_facility: 'Kamuzu Central Hospital', email_address: 'area25@gmail.com', phone_number: '+265123534512', type_code: 'health centre', latitude: '33.77465', longitude: '-13.89272', status: 'Functional', service_id: '1', created_by: 'Blessings', approved_by: 'Lyton',created_at:  '2017-10-10', updated_at: '2017-10-10');
 Facility.create(facility_id: 'DA04001', name: 'chankhungu', description: 'This is in dowa', cell_location: 'dowa', closing_date: '2070-10-10', opening_date: '2017
 	-10-10', parent_facility: 'Dowa district hospital', email_address: 'chamkhungua@gmail.com', phone_number: '+26512345612', type_code: 'dispensary', latitude: '33.77465', longitude: '-13.89272', status: 'Functional', service_id: '1', created_by: 'Blessings', approved_by: 'Lyton',created_at:  '2017-10-10', updated_at: '2017-10-10');
 Facility.create(facility_id: 'MZ02003', name: 'Mzimba district hospital', description: 'This is in Mzimba', cell_location: 'Mzimba', closing_date: '2070-10-10', opening_date: '2017
 	-10-10', parent_facility: 'Mzuzu Central Hospital', email_address: 'mzdh@gmail.com', phone_number: '+26512345612', type_code: 'hospital', latitude: '33.58421', longitude: '-11.89805', status: 'Functional', service_id: '1', created_by: 'Blessings', approved_by: 'Lyton',created_at:  '2017-10-10', updated_at: '2017-10-10');
 
-FacilityUtility.create(facility_id: 'll01002', utility_id: '2', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01002', utility_id: '3', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01002', utility_id: '9', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01001', utility_id: '8', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01002', utility_id: '1', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01002', utility_id: '11', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01002', utility_id: '7', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01002', utility_id: '8', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01002', utility_id: '2', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01002', utility_id: '3', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01002', utility_id: '9', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01001', utility_id: '8', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01002', utility_id: '1', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01002', utility_id: '11', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01002', utility_id: '7', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01002', utility_id: '8', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'DA04001', utility_id: '2', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'DA04001', utility_id: '3', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'DA04001', utility_id: '9', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01001', utility_id: '8', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01001', utility_id: '8', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'DA04001', utility_id: '1', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'DA04001', utility_id: '11', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'DA04001', utility_id: '7', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
@@ -55,7 +55,7 @@ FacilityUtility.create(facility_id: 'DA04001', utility_id: '8', utility_descript
 FacilityUtility.create(facility_id: 'MZ02003', utility_id: '2', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'MZ02003', utility_id: '3', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'MZ02003', utility_id: '9', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityUtility.create(facility_id: 'll01001', utility_id: '8', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityUtility.create(facility_id: 'LL01001', utility_id: '8', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'MZ02003', utility_id: '1', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'MZ02003', utility_id: '11', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityUtility.create(facility_id: 'MZ02003', utility_id: '7', utility_description: 'This is in use', utility_status: 'operational',created_at: '2017-10-10', updated_at: '2017-10-10')
@@ -78,21 +78,21 @@ FacilityService.create(facility_id:'DA04001', service_id: '2',service_descriptio
 FacilityService.create(facility_id:'DA04001', service_id: '4',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 
 FacilityService.create(facility_id:'DA04001', service_id: '5',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityService.create(facility_id:'ll01002', service_id: '1',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityService.create(facility_id:'LL01002', service_id: '1',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityService.create(facility_id:'ll01002', service_id: '2',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityService.create(facility_id:'LL01002', service_id: '2',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityService.create(facility_id:'ll01002', service_id: '4',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityService.create(facility_id:'LL01002', service_id: '4',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityService.create(facility_id:'ll01002', service_id: '5',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityService.create(facility_id:'LL01002', service_id: '5',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityService.create(facility_id:'ll01001', service_id: '1',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityService.create(facility_id:'LL01001', service_id: '1',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityService.create(facility_id:'ll01001', service_id: '2',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityService.create(facility_id:'LL01001', service_id: '2',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityService.create(facility_id:'ll01001', service_id: '4',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityService.create(facility_id:'LL01001', service_id: '4',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityService.create(facility_id:'ll01001', service_id: '5',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityService.create(facility_id:'LL01001', service_id: '5',service_description: 'The service is being provided', created_at: '2017-10-10', updated_at: '2017-10-10')
 Location.create(zone: 'Central West', district: 'Lilongwe',area: '10000', created_at: '2017-10-10', updated_at: '2017-10-10')
 
 Location.create(zone: 'Central West', district: 'Dedza',area: '10000', created_at: '2017-10-10', updated_at: '2017-10-10')
@@ -110,9 +110,9 @@ Location.create(zone: 'Central East', district: 'Ntchisi',area: '10000', created
 Location.create(zone: 'Central East', district: 'salima
 	',area: '10000', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityLocation.create(facility_id: 'll01001',location_id: '1', population: '10000',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityLocation.create(facility_id: 'LL01001',location_id: '1', population: '10000',created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityLocation.create(facility_id: 'll01002',location_id: '1', population: '10000',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityLocation.create(facility_id: 'LL01002',location_id: '1', population: '10000',created_at: '2017-10-10', updated_at: '2017-10-10')
 
 FacilityLocation.create(facility_id: 'DA04001',location_id: '1', population: '10000',created_at: '2017-10-10', updated_at: '2017-10-10')
 
@@ -125,9 +125,9 @@ Partner.create(name: 'EGPAF', origin: 'United Kingdom', created_at: '2017-10-10'
 
 Partner.create(name: 'BHT', origin: 'America', created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityPartner.create(facility_id: 'll01001', partner_id: '1')
+FacilityPartner.create(facility_id: 'LL01001', partner_id: '1')
 
-FacilityPartner.create(facility_id: 'll01002', partner_id: '2')
+FacilityPartner.create(facility_id: 'LL01002', partner_id: '2')
 
 FacilityPartner.create(facility_id: 'DA04001', partner_id: '3')
 
@@ -139,9 +139,9 @@ Ownership.create(name: 'Private',created_at: '2017-10-10', updated_at: '2017-10-
 
 Ownership.create(name: 'Company',created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityOwnership.create(facility_id: 'll01001',ownership_id: '1', ownership_type: 'parastatal',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityOwnership.create(facility_id: 'LL01001',ownership_id: '1', ownership_type: 'parastatal',created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityOwnership.create(facility_id: 'll01002',ownership_id: '1', ownership_type: 'parastatal',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityOwnership.create(facility_id: 'LL01002',ownership_id: '1', ownership_type: 'parastatal',created_at: '2017-10-10', updated_at: '2017-10-10')
 
 FacilityOwnership.create(facility_id: 'DA04001',ownership_id: '1', ownership_type: 'parastatal',created_at: '2017-10-10', updated_at: '2017-10-10')
 
@@ -161,15 +161,15 @@ Resource.create(name: 'Ambulances',created_at: '2017-10-10', updated_at: '2017-1
 Resource.create(name: 'Freezers',created_at: '2017-10-10', updated_at: '2017-10-10')
 Resource.create(name: 'Computers',created_at: '2017-10-10', updated_at: '2017-10-10')
 
-FacilityResource.create(facility_id: 'll01001',resource_id: '1',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityResource.create(facility_id: 'll01001',resource_id: '2',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityResource.create(facility_id: 'll01001',resource_id: '3',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityResource.create(facility_id: 'll01001',resource_id: '4',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityResource.create(facility_id: 'll01002',resource_id: '2',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityResource.create(facility_id: 'll01002',resource_id: '1',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityResource.create(facility_id: 'll01002',resource_id: '2',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityResource.create(facility_id: 'll01002',resource_id: '3',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
-FacilityResource.create(facility_id: 'll01002',resource_id: '4',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01001',resource_id: '1',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01001',resource_id: '2',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01001',resource_id: '3',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01001',resource_id: '4',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01002',resource_id: '2',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01002',resource_id: '1',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01002',resource_id: '2',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01002',resource_id: '3',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
+FacilityResource.create(facility_id: 'LL01002',resource_id: '4',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
 
 FacilityResource.create(facility_id: 'DA04001',resource_id: '1',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
 FacilityResource.create(facility_id: 'DA04001',resource_id: '2',resource_quantity: '5',resource_description: 'This is functional',created_at: '2017-10-10', updated_at: '2017-10-10')
