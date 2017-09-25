87233c4f3d69f314897a0c678b22be6c1a9836ce
Blessings Nthezemu Kamanga <blessnkamanga@gmail.com>
2017-09-25 16:41:28 +0200

    have worked on the landing page to have the services coming from the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :get_all_facilities
+  before_action :get_provided_services
   # get all facilities  
   def get_all_facilities
   	  @allfacilities = Facility.all
@@ -19,7 +20,34 @@ class ApplicationController < ActionController::Base
       #This can be written as follows:
       # session[:login_status] = 'not logged in' unless session[:login_status] == 'logged in'
       # @allfacilities = Facility.all
-
+    
   	  
+  end
+  def get_provided_services
+     @surgery = 0
+     @fp = 0
+     @maternity = 0
+     @dental = 0
+     @other = 0
+     @allservices = 0
+     services_provided = FacilityService.all
+     services_provided.each do |s|
+     case s.service_id  
+
+     when 1
+      @surgery += 1
+    when 2
+      @fp += 1
+    when 3
+      @maternity += 1
+    when 4
+      @dental += 1
+    else
+      @other += 1
+    end
+    @allservices += (@surgery + @fp + @maternity + @dental + @other)
+      
+    end
+    
   end
 end
